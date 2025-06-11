d72a4698a83eab774329c92d0b06625ec0b518f2
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2025-06-11 19:06:40 +0300

    add exercise

---
+++ src/main/java/bg/softuni/oneMonthPreparationKit/countingSort1/Solution.java
@@ -0,0 +1,54 @@
+package bg.softuni.oneMonthPreparationKit.countingSort1;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        int[] frequency = new int[100]; // fixed size array for 0–99
+
+        for (int num : arr) {
+            frequency[num]++;
+        }
+
+        return Arrays.stream(frequency).boxed().collect(Collectors.toList());
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
